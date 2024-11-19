392d52160a0fb824babda18fc0418b8e4ecd57da
jelenu <jesusleon2700@gmail.com>
2024-11-19 14:34:56 +0100

    add token verify view

--- ProjectManager/urls.py
+++ ProjectManager/urls.py
@@ -11,5 +11,7 @@ urlpatterns = [
 
     path('auth/', include('djoser.urls')),
     path('auth/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('auth/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'), 
+    path('auth/token/verify/', jwt_views.TokenVerifyView.as_view(), name='token_verify'),
+
 ]
